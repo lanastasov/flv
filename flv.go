e138f13c5469148dfc104fc5d9bed62a2499d75a
Lubomir Anastasov <lubomir.anastasov@gmail.com>
2016-07-28 21:04:20 +0300

    flv.go

---
+++ flv.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"path/filepath"
+	"os/exec"
+)
+
+func main() {
+	files, _ := filepath.Glob("*")
+	for i := range files {
+		fmt.Println("  <-  ", files[i])
+		extensions := []string { ".flv", ".wmv" , ".FLV", ".WMV"}
+		for j := range extensions {
+			if strings.Contains(files[i], extensions[j]) {
+				cmd, err := exec.Command("ffmpeg", "-i", fmt.Sprintf(files[i]), strings.Replace(fmt.Sprintf(files[i]),extensions[j], ".mp4", 1)).Output()
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+			fmt.Printf("Command ran: %s\n", string(cmd))
+			}	
+		}
+	}
+}
