cdb892a80bc14510523a003d69ec1e27c619ed82
Lubomir Anastasov <lubomir.anastasov@gmail.com>
2016-07-28 21:40:30 +0300

    .avi

--- flv.go
+++ flv.go
@@ -11,7 +11,7 @@ func main() {
 	files, _ := filepath.Glob("*")
 	for i := range files {
 		fmt.Println("  <-  ", files[i])
-		extensions := []string { ".flv", ".wmv" , ".FLV", ".WMV"}
+		extensions := []string { ".flv", ".wmv" , ".FLV", ".WMV", ".avi", ".AVI"}
 		for j := range extensions {
 			if strings.Contains(files[i], extensions[j]) {
 				cmd, err := exec.Command("ffmpeg", "-i", fmt.Sprintf(files[i]), strings.Replace(fmt.Sprintf(files[i]),extensions[j], ".mp4", 1)).Output()
