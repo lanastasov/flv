75a56eddb180a7fd03852129ee2f16ac47140b91
Lubomir Anastasov <lubomir.anastasov@gmail.com>
2016-07-30 14:08:05 +0300

    libx264

--- flv.go
+++ flv.go
@@ -14,7 +14,7 @@ func main() {
 		extensions := []string { ".flv", ".wmv" , ".FLV", ".WMV", ".avi", ".AVI"}
 		for j := range extensions {
 			if strings.Contains(files[i], extensions[j]) {
-				cmd, err := exec.Command("ffmpeg", "-i", fmt.Sprintf(files[i]), strings.Replace(fmt.Sprintf(files[i]),extensions[j], ".mp4", 1)).Output()
+				cmd, err := exec.Command("ffmpeg", "-i", fmt.Sprintf(files[i]), "-vcodec", "libx264", strings.Replace(fmt.Sprintf(files[i]),extensions[j], ".mp4", 1)).Output()
 				if err != nil {
 					fmt.Println(err)
 					return
