e880536515020b980d8d1867ffe0f66305ca01e3
lanastasov <lubomir.anastasov@gmail.com>
2019-03-31 20:09:48 +0300

    Update flv.go

--- flv.go
+++ flv.go
@@ -11,7 +11,7 @@ func main() {
 	files, _ := filepath.Glob("*")
 	for i := range files {
 		fmt.Println("  <-  ", files[i])
-		extensions := []string { ".flv", ".wmv" , ".FLV", ".WMV", ".avi", ".AVI", "mkv", ".ts", ".TS"}
+		extensions := []string { ".flv", ".wmv" , ".FLV", ".WMV", ".avi", ".AVI", "mkv", ".ts", ".TS", ".mpg", ".MPG"}
 		for j := range extensions {
 			if strings.Contains(files[i], extensions[j]) {
 				cmd, err := exec.Command("ffmpeg", "-i", fmt.Sprintf(files[i]), "-vcodec", "libx264", strings.Replace(fmt.Sprintf(files[i]),extensions[j], ".mp4", 1)).Output()
